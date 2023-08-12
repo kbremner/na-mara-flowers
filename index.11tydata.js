afc089ffa47ca39613f8c92e70393c84f0abb603
Kyle Bremner <kb.bremner@gmail.com>
2023-08-12 23:49:47 +0100

    first commit

---
+++ index.11tydata.js
@@ -0,0 +1,12 @@
+const client = require("./_lib/contentful");
+
+module.exports = async function () {
+  console.log("Fetching main page content...");
+  const result = await client.getEntries({
+    content_type: "mainPage",
+    limit: 1,
+  });
+  console.log("Main page content fetched");
+  const data = result.items[0].fields;
+  return { callouts: data.callouts };
+};
