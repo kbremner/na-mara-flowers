bd90fd8624cf851085fd11d557c7eb455feacc77
Kyle Bremner <kb.bremner@gmail.com>
2023-08-13 13:25:18 +0100

    Supports previewing content locally

--- _lib/contentful.js
+++ _lib/contentful.js
@@ -3,4 +3,7 @@ module.exports = require("contentful").createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: `${
+    process.env.CONTENTFUL_ENABLE_PREVIEW === "true" ? "preview" : "cdn"
+  }.contentful.com`,
 });
